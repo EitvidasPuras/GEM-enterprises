8094674918a92c7b410e360bec011e68daa6f97d
MariusDura <termosroviukas@gmail.com>
2017-10-19 19:01:56 +0300

    Conflict fix

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -32,40 +32,44 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li>
-                            <a href="/shoppingcart"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping
-                                cart</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
-                                <span class="glyphicon glyphicon-user"></span>
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li>
+                        <a href="/shoppingcart"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping
+                            cart</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false">
+                            <span class="glyphicon glyphicon-user"></span>
 
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            @if(Auth::user()->hasRole('Admin'))
                                 <li>
-                                    <a href="/settings">Settings</a>
+                                    <a href="{{route('admin')}}">Admin panel</a>
                                 </li>
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                            @endif
+                            <li>
+                                <a href="/settings">Settings</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+                                    Logout
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                        @endguest
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                    @endguest
             </ul>
         </div>
     </div>
