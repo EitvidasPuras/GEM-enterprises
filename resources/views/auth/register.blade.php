2bcaf81093ddce57a82fe83bebab2cc9a88d3913
Eitvidas Puras <peitvis@gmail.com>
2017-11-08 16:59:22 +0200

    Changed registration First name and Last name input fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label for="name" class="col-md-4 control-label">Name</label>
+                                <label for="name" class="col-md-4 control-label">First name</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control" name="name"
